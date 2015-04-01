59a1caeeeec613f450905ee5b4929d3cf82c0eaf
markos0808 <markos@foi.hr>
2015-04-01 20:01:34 +0200

    Povezano grafičko sučelje sa logikom
    Kreirane Event Handler metode za gumbe Uplati i Odigraj. Preuzimaju se
    korisnički podaci sa forme, prosljeđuju objektu klase Loto, te se
    rezultati obrade prikazuju na formi.

--- LOTO aplikacija/LOTO aplikacija/Form1.Designer.cs
+++ LOTO aplikacija/LOTO aplikacija/Form1.Designer.cs
@@ -116,6 +116,7 @@
             this.btnUplati.TabIndex = 8;
             this.btnUplati.Text = "Uplati";
             this.btnUplati.UseVisualStyleBackColor = true;
+            this.btnUplati.Click += new System.EventHandler(this.btnUplati_Click);
             // 
             // groupBoxDobitni
             // 
@@ -191,6 +192,7 @@
             this.btnOdigraj.TabIndex = 17;
             this.btnOdigraj.Text = "Odigraj";
             this.btnOdigraj.UseVisualStyleBackColor = true;
+            this.btnOdigraj.Click += new System.EventHandler(this.btnOdigraj_Click);
             // 
             // lblNatpis
             // 
