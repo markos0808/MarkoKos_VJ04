e6c4db99f57c4495814d36881144b506a490bb1a
markos0808 <markos@foi.hr>
2015-05-07 13:07:17 +0200

    Banka
    Aplikacija bankomat

---
+++ Banka/Banka/Form1.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace Banka
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtBrojRacuna = new System.Windows.Forms.TextBox();
+            this.txtPIN = new System.Windows.Forms.TextBox();
+            this.btnPrijava = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtIznos = new System.Windows.Forms.TextBox();
+            this.txtStanjeRacuna = new System.Windows.Forms.TextBox();
+            this.btnUnesiIznos = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnPrijava);
+            this.groupBox1.Controls.Add(this.txtPIN);
+            this.groupBox1.Controls.Add(this.txtBrojRacuna);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(0, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(316, 100);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Prijava";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(12, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Broj računa:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(12, 65);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "PIN:";
+            // 
+            // txtBrojRacuna
+            // 
+            this.txtBrojRacuna.Location = new System.Drawing.Point(94, 23);
+            this.txtBrojRacuna.Name = "txtBrojRacuna";
+            this.txtBrojRacuna.Size = new System.Drawing.Size(100, 20);
+            this.txtBrojRacuna.TabIndex = 2;
+            // 
+            // txtPIN
+            // 
+            this.txtPIN.Location = new System.Drawing.Point(94, 62);
+            this.txtPIN.Name = "txtPIN";
+            this.txtPIN.Size = new System.Drawing.Size(100, 20);
+            this.txtPIN.TabIndex = 3;
+            // 
+            // btnPrijava
+            // 
+            this.btnPrijava.Location = new System.Drawing.Point(220, 42);
+            this.btnPrijava.Name = "btnPrijava";
+            this.btnPrijava.Size = new System.Drawing.Size(75, 23);
+            this.btnPrijava.TabIndex = 4;
+            this.btnPrijava.Text = "Prijava";
+            this.btnPrijava.UseVisualStyleBackColor = true;
+            this.btnPrijava.Click += new System.EventHandler(this.btnPrijava_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btnUnesiIznos);
+            this.groupBox2.Controls.Add(this.txtStanjeRacuna);
+            this.groupBox2.Controls.Add(this.txtIznos);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Location = new System.Drawing.Point(0, 106);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(316, 103);
+            this.groupBox2.TabIndex = 5;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Podizanje novaca";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label3.Location = new System.Drawing.Point(12, 29);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Iznos:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label4.Location = new System.Drawing.Point(12, 68);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(90, 13);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Stanje računa:";
+            // 
+            // txtIznos
+            // 
+            this.txtIznos.Location = new System.Drawing.Point(108, 26);
+            this.txtIznos.Name = "txtIznos";
+            this.txtIznos.Size = new System.Drawing.Size(100, 20);
+            this.txtIznos.TabIndex = 2;
+            // 
+            // txtStanjeRacuna
+            // 
+            this.txtStanjeRacuna.Location = new System.Drawing.Point(108, 65);
+            this.txtStanjeRacuna.Name = "txtStanjeRacuna";
+            this.txtStanjeRacuna.Size = new System.Drawing.Size(100, 20);
+            this.txtStanjeRacuna.TabIndex = 3;
+            // 
+            // btnUnesiIznos
+            // 
+            this.btnUnesiIznos.Location = new System.Drawing.Point(220, 45);
+            this.btnUnesiIznos.Name = "btnUnesiIznos";
+            this.btnUnesiIznos.Size = new System.Drawing.Size(75, 23);
+            this.btnUnesiIznos.TabIndex = 4;
+            this.btnUnesiIznos.Text = "Unesi iznos";
+            this.btnUnesiIznos.UseVisualStyleBackColor = true;
+            this.btnUnesiIznos.Click += new System.EventHandler(this.btnUnesiIznos_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(328, 220);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Bankomat";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnPrijava;
+        private System.Windows.Forms.TextBox txtPIN;
+        private System.Windows.Forms.TextBox txtBrojRacuna;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnUnesiIznos;
+        private System.Windows.Forms.TextBox txtStanjeRacuna;
+        private System.Windows.Forms.TextBox txtIznos;
+    }
+}
+
---
+++ Banka/Banka/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Banka
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            MessageBox.Show("Bankomat je spreman za korištenje!");
+        }
+
+        private void btnPrijava_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnUnesiIznos_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
